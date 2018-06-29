ca789b5b029fd92b1a15ca4b955afdc5caa6380e
mactive <mactive@gmail.com>
2018-06-29 08:01:53 +0800

    tree

--- src/routes/HumanRes/Tree.js
+++ src/routes/HumanRes/Tree.js
@@ -1,118 +1,273 @@
 import React, { Fragment } from 'react';
-import { Button, Row, Col, Icon, message, Tree, Card, Input, Upload } from 'antd';
+import {
+  Button,
+  Row,
+  Col,
+  Icon,
+  message,
+  Tree,
+  Card,
+  Input,
+  Upload,
+  Modal,
+  Form,
+  Checkbox,
+} from 'antd';
 import Result from 'components/Result';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import { routerRedux } from 'dva/router';
 import { connect } from 'dva';
 const TreeNode = Tree.TreeNode;
+const CheckboxGroup = Checkbox.Group;
+import styles from './Tree.less';
 
-class Step1 extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-  }
+const treeData = [
+  {
+    title: '无锡片区',
+    key: '0-0',
+    children: [
+      {
+        title: '常州分公司',
+        key: '0-0-0',
+        children: [
+          { title: '财务: 王XX', key: '0-0-0-0' },
+          { title: '行政: 李XX', key: '0-0-0-1' },
+          { title: '库管: 齐XX', key: '0-0-0-2' },
+        ],
+      },
+      {
+        title: '泰州分公司',
+        key: '0-0-1',
+        children: [
+          { title: '财务: 王XX', key: '0-0-1-0' },
+          { title: '行政: 李XX', key: '0-0-1-1' },
+          { title: '库管: 齐XX', key: '0-0-1-2' },
+        ],
+      },
+      {
+        title: '嘉兴分公司',
+        key: '0-0-2',
+        children: [{ title: '财务: 王XX', key: '0-0-2-0' }],
+      },
+      {
+        title: '湖州分公司',
+        key: '0-0-3',
+        children: [{ title: '财务: 王XX', key: '0-0-3-0' }],
+      },
+      {
+        title: '南通分公司',
+        key: '0-0-4',
+        children: [{ title: '财务: 王XX', key: '0-0-4-0' }],
+      },
+    ],
+  },
+  {
+    title: '河南及湖北片区',
+    key: '0-1',
+    children: [
+      {
+        title: '襄阳分公司',
+        key: '0-1-0',
+      },
+      {
+        title: '宜昌分公司',
+        key: '0-1-1',
+      },
+      {
+        title: '洛阳分公司',
+        key: '0-1-2',
+      },
+      {
+        title: '嘉里物流（郑州）',
+        key: '0-1-3',
+      },
+      {
+        title: '河南分公司',
+        key: '0-1-4',
+      },
+    ],
+  },
+  {
+    title: '青岛片区',
+    key: '0-3',
+    children: [{ title: 'xx分公司', key: '0-3-0' }],
+  },
+  {
+    title: '宁波片区',
+    key: '0-4',
+    children: [{ title: 'xx分公司', key: '0-4-0' }],
+  },
+  {
+    title: '重点分公司',
+    key: '0-5',
+    children: [{ title: 'xx分公司', key: '0-5-0' }],
+  },
+];
 
+const plainOptions = ['采购权', '审批权', '人事权'];
+
+@Form.create()
+class Demo extends React.Component {
+  state = {
+    expandedKeys: ['0-0-0', '0-0-1'],
+    autoExpandParent: true,
+    checkedKeys: ['0-0-0'],
+    selectedKeys: [],
+    visible: false,
+    modalBVisible: false,
+    confirmLoading: false,
+  };
+  onExpand = expandedKeys => {
+    // console.log('onExpand', arguments);
+    // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+    // or, you can remove all expanded children keys.
+    this.setState({
+      expandedKeys,
+      autoExpandParent: false,
+    });
+  };
+  onCheck = checkedKeys => {
+    console.log('onCheck', checkedKeys);
+    this.setState({ checkedKeys });
+  };
   onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
-  }
-  onCheck = (checkedKeys, info) => {
-    console.log('onCheck', checkedKeys, info);
-  }
+    console.log('onSelect', info);
+    if (info.node.getNodeChildren().length > 0) {
+      this.setState({
+        selectedKeys,
+        modalBVisible: true,
+      });
+    } else {
+      this.setState({
+        selectedKeys,
+        visible: true,
+      });
+    }
+  };
+
+  handleOk = () => {
+    this.setState({
+      confirmLoading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false,
+        modalBVisible: false,
+      });
+    }, 2000);
+  };
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+      modalBVisible: false,
+    });
+  };
+  renderTreeNodes = data => {
+    return data.map(item => {
+      if (item.children) {
+        return (
+          <TreeNode
+            title={
+              <span style={{ color: '#333' }}>
+                {' '}
+                {item.title}{' '}
+                <span className={styles.actions}>
+                  <a>导出</a> - <a>导入替换</a>{' '}
+                </span>{' '}
+              </span>
+            }
+            key={item.key}
+            dataRef={item}
+          >
+            {this.renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return (
+        <TreeNode
+          {...item}
+          title={
+            <span style={{ color: '#666' }}>
+              {' '}
+              {item.title}{' '}
+              <span className={styles.actions}>
+                <a>编辑</a> - <a>删除</a>{' '}
+              </span>
+            </span>
+          }
+        />
+      );
+    });
+  };
 
   render() {
-    const { dispatch } = this.props;
-    const architecture = [
-      {
-        title: "无锡片区", key: '0-0-0', child: [
-          { title: '常州分公司' },
-          { title: '泰州分公司' },
-          { title: '嘉兴分公司' },
-          { title: '湖州分公司' },
-          { title: '南通分公司' },
-        ]
-      },
-      {
-        title: "河南及湖北片区", key: '0-1-0', child: [
-          { title: '襄阳分公司' },
-          { title: '宜昌分公司' },
-          { title: '洛阳分公司' },
-          { title: '嘉里物流（郑州）' },
-          { title: '河南分公司' },
-        ]
-      },
-      {
-        title: "青岛片区", child: [
-          { title: '青岛分公司' },
-          { title: '东营分公司' },
-          { title: '淄博分公司' },
-          { title: '黄岛物流' },
-          { title: '潍坊分公司' },
-          { title: '临沂分公司' },
-          { title: '前湾嘉里大通' },
-        ]
-      },
-      {
-        title: "宁波片区", child: [
-          { title: '宁波分公司' },
-          { title: '宁波保税子公司' },
-          { title: '绍兴分公司' },
-        ]
-      },
-      {
-        title: "重点分公司", child: [
-          { title: '昆山分公司' },
-          { title: '湖北分公司' },
-          { title: '山东分公司' },
-          { title: '烟台分公司' },
-          { title: '安徽分公司' },
-          { title: '杭州分公司' },
-          { title: '威海分公司' },
-          { title: '济宁分公司' },
-        ]
-      },
-    ];
-    const extra = (
-      <Fragment>
-        <Tree
-          checkable
-          defaultExpandedKeys={['0-0-0', '0-1-0']}
-          // defaultSelectedKeys={['0-0-0', '0-0-1']}
-          // defaultCheckedKeys={['0-0-0', '0-0-1']}
-          onSelect={this.onSelect}
-          onCheck={this.onCheck}
-        >
-          {
-            architecture.map((node) => {
-              return (
-                <TreeNode key={node.key} title={node.title}>
-                  {node.child.map(cnode => {
-                    return (<TreeNode title={
-                      <div>{cnode.title}
-                        <span style={{ color: '#1890ff' }}> <a>导出</a> - <a>编辑</a> -  <a>删除</a> </span>
-                      </div>
-                    } >
-                    </TreeNode>)
-                  })}
-                </TreeNode>
-              )
-            })
-          }
-        </Tree>
-      </Fragment>
-    );
+    const { getFieldDecorator } = this.props.form;
 
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
 
     return (
       <PageHeaderLayout>
         <Card bordered={false} title="组织架构">
-          {extra}
+          <Tree
+            checkable
+            onExpand={this.onExpand}
+            expandedKeys={this.state.expandedKeys}
+            autoExpandParent={this.state.autoExpandParent}
+            onCheck={this.onCheck}
+            checkedKeys={this.state.checkedKeys}
+            onSelect={this.onSelect}
+            selectedKeys={this.state.selectedKeys}
+          >
+            {this.renderTreeNodes(treeData)}
+          </Tree>
         </Card>
+        <Modal
+          title="编辑"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          confirmLoading={this.state.confirmLoading}
+          onCancel={this.handleCancel}
+        >
+          <Form>
+            <Form.Item {...formItemLayout} label="姓名">
+              <Input value="李xx" />
+            </Form.Item>
+            <Form.Item {...formItemLayout} label="电话">
+              <Input type="text" value="18611009876" />
+            </Form.Item>
+            <Form.Item {...formItemLayout} label="邮箱">
+              <Input type="text" value="ziqi.liu@kerry.com.cn" />
+            </Form.Item>
+            <Form.Item {...formItemLayout} label="权限">
+              <CheckboxGroup options={plainOptions} defaultValue={['采购权']} />
+            </Form.Item>
+          </Form>
+        </Modal>
+        <Modal
+          title="导入新人员信息"
+          visible={this.state.modalBVisible}
+          onOk={this.handleOk}
+          confirmLoading={this.state.confirmLoading}
+          onCancel={this.handleCancel}
+        >
+          <p>批量导入人员信息</p>
+          <Button>
+            <Icon type="upload" /> 上传
+          </Button>
+        </Modal>
       </PageHeaderLayout>
-    )
+    );
   }
 }
 
-export default connect(({ form }) => ({
-  form
-}))(Step1);
+export default connect(({ form }) => ({}))(Demo);
